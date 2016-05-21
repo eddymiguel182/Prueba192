b94c2ea26d14737dd04911fea8682d87cc25b243
SERVIDOR2 <SERVIDOR2@SERVIDOR2-PC>
2016-05-20 21:32:26 -0600

    prueba hoy pagina web

---
+++ Eddy-Prueba/Eddy-Prueba/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Eddy_Prueba.Global" Language="C#" %>
---
+++ Eddy-Prueba/Eddy-Prueba/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Eddy_Prueba.Startup))]
+namespace Eddy_Prueba
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
